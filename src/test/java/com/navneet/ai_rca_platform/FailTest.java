5ddcc1a823c49723e11554116ff431952b4489b1
aznavneet <navneetbhardwaj935@gmail.com>
2026-06-23 14:00:40 +0000

    test case

---
+++ src/test/java/com/navneet/ai_rca_platform/FailTest.java
@@ -0,0 +1,12 @@
+package com.navneet.ai_rca_platform;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FailTest {
+
+    @Test
+    void shouldFail() {
+        assertEquals(1, 2);
+    }
+}
